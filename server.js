ceb6e6513a5bc5503bdd9c1db62ffc1921944878
hazmick <93008407+hazmick@users.noreply.github.com>
2023-12-30 21:25:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ require('dotenv').config({ path: "./config.env"})
 const errorHandler = require("./middleware/errorHandler");
 const express = require('express');
 const connection = require("./config/db");
+const cors = require('cors');
+
 connection;
+
+const allowedorigins = ['https://hazmick.xyz', 'https://www.hazmick.xyz'];
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: allowedorigins
+}))
 
 app.use("/api/auth", require('./routes/auth'));
 app.use("/private", require("./routes/private"));
@@ -15,6 +22,6 @@ app.use("/ping", require("./routes/ping"));
 
 app.use(errorHandler);
 const PORT = 5000;
- 
+
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
